f2698522b864809a361c1f7f7248e7a233139bfd
Alejandro Tejada <tej17584@uvg.edu.gt>
2021-10-08 20:26:07 -0600

    nodo de bool

---
+++ nodoBoolean.py
@@ -0,0 +1,69 @@
+"""
+Nombre: Alejandro Tejada
+Curso: Diseño Compiladores
+Fecha: septiembre 2021
+Programa: NodoBoolean.py
+Propósito: Este programa aloja el nodo para el bool
+V 2.0
+"""
+
+
+class NodoBooleano():
+
+    def __init__(self) -> None:
+        self.code = ""
+        self.siguiente = ""
+        self.trueValue = ""
+        self.falseValue = ""
+
+    def getCode(self):
+        """
+        Retorna el codigo del nodo
+        """
+        return self.code
+
+    def setCode(self, codigo):
+        """
+        Agrega codigo al nodito
+        *@param: codigo: el codigo a agregar al nodito
+        """
+        self.code = codigo
+
+    def getSiguiente(self):
+        """
+        Retorna el siguiente
+        """
+        return self.siguiente
+
+    def setSiguiente(self, next):
+        """
+        Agrega el next
+        *@param: next: el next a agregar al nodito
+        """
+        self.code = next
+
+    def getTrue(self):
+        """
+        Retorna el true
+        """
+        return self.trueValue
+
+    def setTrue(self, value):
+        """
+        Agrega el true
+        *@param: value: el nuevo valor
+        """
+        self.trueValue = value
+
+    def getFalse(self):
+        """
+        Retorna el false
+        """
+        return self.falseValue
+
+    def setTrue(self, value):
+        """
+        Agrega el false
+        *@param: value: el nuevo valor false
+        """
+        self.falseValue = value
